dc479403a35eaad44262f0340208a6d08fd4b08f
Серж <93073993+MSQTL@users.noreply.github.com>
2023-02-14 14:42:25 +0300

    2 + 3

--- Лабораторная работа №3.py
+++ Лабораторная работа №3.py
@@ -24,3 +24,24 @@ def ex2():
                 nums[j + 1] = temp
 
     print(nums)
+
+    
+    def ex3():
+    nums = list()
+    sum = 0
+
+    while True:
+        num = int(input("Введите число: "))
+        sum += num
+        if sum == 0:
+            nums.append(num)
+            break
+        else:
+            nums.append(num)
+    sum = 0
+    for i in nums:
+        sum += i*i
+    print(sum)
+    
+    
+    
